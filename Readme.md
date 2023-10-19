7ee59dd7356ff70f0dad84a44946574d8e301464
b06b01073 <b06b01073@gmail.com>
2023-10-19 16:18:50 +0800

    official's comparison

--- Readme.md
+++ Readme.md
@@ -3,15 +3,25 @@
 ## Introduction
 This is an implementation of Visional Transformer from the paper [An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale](https://arxiv.org/abs/2010.11929) using Pytorch. The model is tested on the MNIST dataset and the Cifar-10 dataset.
 
-The model is trained for 10 epochs on the MNIST dataset, and 150 epochs on the Cifar-10 dataset.
+The model is trained until it stop improving on the test set for a consecutive `--patience` epochs (`patience=10` for the result below), or the training reaches the maximum training epochs.
 
 
 
 ## Result
 
-The results are obtained using the default hyperparameters in the `train.py` file. The only modification is the patch size (`-p`). To ensure the image height and weight are divisible by the patch size, you are required to specify a suitable `-p`, otherwise an error will be thrown. 
+The results are obtained using the default hyperparameters in the `train.py` file. The only modification is the patch size (`-p`). To ensure the image height and weight are divisible by the patch size, you are required to specify a suitable `-p`, otherwise an error will be thrown (we use `-p=7` for MNIST and `-p=8` for Cifar10). 
 
-|     |test acc |
-|-----|--------|
-|MNIST|0.9795     |
-|Cifar-10|0.8061      |
+If you want to compare the performance yourself and the channel of the input image is not 3, then you are required to modify the `VisionTransformer` class in `torchvision.models.vision_transformer` by adding an addtional `in_channels` argument. 
+
+
+### Test accurary
+
+
+|     | This repo | Pytorch official's implementation |
+|-----|--------|------|
+|MNIST|0.9830     |0.9702|
+|Cifar-10|0.7860    | 0.7576
+
+> The result is by no means to tell my implementation is better than or worse than the Pytorch official's implementation. This result only suggests that the implementation is highly possible to be correct (there's no bugs that makes this model performs poorly, I mean, I hope so).
+
+![akward](https://media.tenor.com/AFJsOL0Ox-AAAAAC/awkward.gif)
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import accuracy_score
+from torchvision.models.vision_transformer import VisionTransformer as TorchViT
 from torchsummary import summary
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -57,7 +58,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--lr', default=1e-4, type=float) # the model is learning-rate sensitive, use smaller learning rate 
     parser.add_argument('--batch_size', '-b', default=512, type=int)
-    parser.add_argument('--epoch', '-e', default=500, type=int)
+    parser.add_argument('--epoch', '-e', default=100, type=int)
     parser.add_argument('--patch_size', '-p', default=7, type=int)
     parser.add_argument('--embedded_dim', '-d', default=384, type=int)
     parser.add_argument('--encoder_layer', '-l', default=6, type=int)
@@ -67,33 +68,52 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', '--ds', default='mnist', type=str, help='Currently support only MNIST(mnist) and CIFAR-10(cifar)')
     parser.add_argument('--weight_decay', '--wd', default=1e-4, type=float)
     parser.add_argument('--label_smoothing', '--ls', default=0, type=float)
+    parser.add_argument('--from_pytorch', action='store_true')
+    parser.add_argument('--patience', type=int, default=10)
 
     args = parser.parse_args()
     
     train_set, test_set, img_shape = dataset.get_dataset(args.batch_size, args.dataset)
 
-    _, h, w = img_shape
-    assert h % args.patch_size == 0 and w % args.patch_size == 0, ('The shape of image is not divisible by the size of patch. This might lead to the lost of information from the image.')
+    c, h, w = img_shape
+    assert h % args.patch_size == 0 and w % args.patch_size == 0, (f'The shape of image({h, w}) is not divisible by the size of patch{args.patch_size}. This might lead to the lost of information from the image.')
 
-    model = ViT(
-        img_shape=img_shape,
-        patch_size=args.patch_size,
-        embedded_dim=args.embedded_dim,
-        encoder_layers=args.encoder_layer,
-        num_class=args.num_class,
-        num_head=args.num_head,
-        drop=args.drop
-    ).to(device)
+    if args.from_pytorch:
+        model = TorchViT(
+            image_size=h,
+            patch_size=args.patch_size,
+            hidden_dim=args.embedded_dim,
+            num_layers=args.encoder_layer,
+            num_classes=args.num_class,
+            num_heads=args.num_head,
+            dropout=args.drop,
+            mlp_dim=args.embedded_dim,
+            in_channels=c,
+        )
+    else:
+        model = ViT(
+            img_shape=img_shape,
+            patch_size=args.patch_size,
+            embedded_dim=args.embedded_dim,
+            encoder_layers=args.encoder_layer,
+            num_class=args.num_class,
+            num_head=args.num_head,
+            drop=args.drop
+        )
 
+    model = model.to(device)
 
-    summary(model, input_size=img_shape)
+    # summary(model, input_size=img_shape)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     loss_fn = nn.CrossEntropyLoss(label_smoothing=args.label_smoothing)
 
     best_acc = 0
 
+    patience_count = 0
     for e in range(args.epoch):
+        patience_count += 1
+
         print(f'epoch: {e}')
 
         train_true, train_preds = train(train_set, model, optimizer, loss_fn)
@@ -105,8 +125,10 @@ if __name__ == '__main__':
         print(f'train_acc: {train_acc:.4f}, test_acc: {test_acc:.4f}')
 
         if test_acc > best_acc:
+            patience_count = 0
             best_acc = test_acc
             print('saving model...')
             torch.save(model.state_dict(), f'{args.dataset}.pth')
 
-            
+        if patience_count >= args.patience:
+            break
